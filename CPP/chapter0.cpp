80dc15c4456011e81e3a31acad611e1475a07787
Azam Malik <azam@sdsol.com>
2013-03-31 01:48:21 -0400

    first

---
+++ CPP/chapter0.cpp
@@ -0,0 +1,9 @@
+//
+//  chapter0.cpp
+//  CPP
+//
+//  Created by Azam Malik on 3/31/13.
+//  Copyright (c) 2013 acceleratedCplusplus. All rights reserved.
+//
+
+#include "chapter0.h"
---
+++ CPP/chapter0.h
@@ -0,0 +1,14 @@
+//
+//  chapter0.h
+//  CPP
+//
+//  Created by Azam Malik on 3/31/13.
+//  Copyright (c) 2013 acceleratedCplusplus. All rights reserved.
+//
+
+#ifndef __CPP__chapter0__
+#define __CPP__chapter0__
+
+#include <iostream>
+
+#endif /* defined(__CPP__chapter0__) */
