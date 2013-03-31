26383b959eff6e24b828df36c27847fc21bfa783
Azam Malik <azam@sdsol.com>
2013-03-31 01:45:53 -0400

    Initial Commit

---
+++ CPP/CPPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPPAppDelegate.h
+//  CPP
+//
+//  Created by Azam Malik on 3/31/13.
+//  Copyright (c) 2013 acceleratedCplusplus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CPP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CPP
+//
+//  Created by Azam Malik on 3/31/13.
+//  Copyright (c) 2013 acceleratedCplusplus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CPPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CPPAppDelegate class]));
+    }
+}
